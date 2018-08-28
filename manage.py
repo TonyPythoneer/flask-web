b0f6ca64a1d56be063af7aa60d718ada7dc07e70
Tony Yang <tonyyang@m800.com>
2018-08-28 16:53:22 +0800

    Add flask-mongo to connect mongodb

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from app import create_app, init_app
+from flask_pymongo import PyMongo
 
 app = create_app()
-init_app(app)
+app = init_app(app)
+
+mongo = PyMongo(app, uri='mongodb://mongodb:27017/myDatabase')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
