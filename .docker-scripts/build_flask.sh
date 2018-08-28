3de011ee78b1dbde4547688e7359e9cb946002c9
Tony Yang <tonyyang@m800.com>
2018-08-28 16:23:39 +0800

    Fix requirement

--- .docker-scripts/build_flask.sh
+++ .docker-scripts/build_flask.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # build venv
-echo "=========888"
-echo $(pwd)
 python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip show pip
 
 # install requirements
-pip install -r requirements.txt
+pip install -r requirements_dev.txt
 
 # keep alive
 echo 'I am alive!'
